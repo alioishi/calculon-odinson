7cbff721f10f702725a4a7b5e36d650aad24da14
alioishi <29942094+alioishi@users.noreply.github.com>
2022-06-27 15:47:39 -0700

    Add display change to button press

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+/*
+    on button press
+        trigger event listener
+            determine button pressed
+            display value
+*/
+
+initButtons();
+
+function initButtons() {
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            changeDisplay(button.textContent)
+        })
+    });
+
+}
+
+function changeDisplay(buttonPress) {
+    const display = document.querySelector("#display");
+
+    display.textContent = buttonPress;
+}
+
 function add(a, b) {
     return a + b;
 }
