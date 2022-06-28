49535dac1806423668a21368e2f06b6c3f78ac95
alioishi <29942094+alioishi@users.noreply.github.com>
2022-06-28 16:18:52 -0700

    Change addToDisplay to modifyCurrentValue and move unary operators into function to address edge cases

--- main.js
+++ main.js
@@ -21,8 +21,8 @@ function initButtons() {
 function changeDisplay(buttonPress) {
     const display = document.querySelector("#display");
 
-    if(buttonPress >= "0" && buttonPress <= "9" || buttonPress == "."){
-        addToDisplay(display, buttonPress);
+    if(buttonPress >= "0" && buttonPress <= "9" || buttonPress == "." || buttonPress == "%" ||buttonPress == "±"){
+        modifyCurrentValue(display, buttonPress);
     }
     else if(buttonPress == "AC"){
         clearDisplay(display);
@@ -32,18 +32,26 @@ function changeDisplay(buttonPress) {
     }
 }
 
-function addToDisplay(display, buttonPress) {
-    if(displayStorage.isBinaryOperatorLastPressed){
-        display.textContent = (buttonPress == ".") ? "0." :
+function modifyCurrentValue(display, buttonPress) {
+    if(buttonPress == "%"){
+        display.textContent = String(display.textContent / 100);
+    }
+    else if(displayStorage.isBinaryOperatorLastPressed){ // replacement of display value after a value is pressed
+        display.textContent = (buttonPress == ".") ? "0." : // addresses edge cases 
                                 (buttonPress == "±") ? "-0" : 
                                 buttonPress;
         displayStorage.isBinaryOperatorLastPressed = false;
     }
-    else if(display.textContent == "0" && buttonPress != "."){
-        display.textContent = buttonPress;
+    else if(buttonPress == "±"){
+        display.textContent = display.textContent.includes("-") ? display.textContent.replace("-", "") : "-" + display.textContent;
     }
-    else if(display.textContent == "-0" && buttonPress != "."){
-        display.textContent = "-" + buttonPress;
+    else if(buttonPress == "."){
+        if(!display.textContent.includes(".")){
+            display.textContent += buttonPress; 
+        }   
+    }
+    else if(display.textContent == 0 && !display.textContent.includes(".")){ // replacement when the displayed value is "0" or "-0"
+            display.textContent = display.textContent.replace("0", buttonPress)
     }
     else{
         display.textContent += buttonPress;
@@ -57,23 +65,14 @@ function clearDisplay(display){
 }
 
 function processOperator(display, buttonPress){
-    if(buttonPress == "%"){
-        display.textContent = String(display.textContent / 100);
+    // allows for multiple presses of binary operators without requiring an equal sign press each time
+    if(displayStorage.storedOperator){
+        display.textContent = String(operate(determineBinaryOperator(displayStorage.storedOperator), 
+                                        displayStorage.storedValue, display.textContent));
     }
-    else if(buttonPress == "±"){
-        display.textContent = (display.textContent == "0") ? "-0" : String(-display.textContent);
-    }
-    else{
-        // allows for multiple presses of binary operators without requiring an equal sign press each time
-        if(displayStorage.storedOperator){
-            display.textContent = String(operate(determineBinaryOperator(displayStorage.storedOperator), 
-                                            displayStorage.storedValue, display.textContent));
-        }
-        displayStorage.storedValue = display.textContent;
-        displayStorage.storedOperator = (buttonPress == "=") ? "" : buttonPress;
-        displayStorage.isBinaryOperatorLastPressed = (buttonPress != "=");
-
-    }
+    displayStorage.storedValue = display.textContent;
+    displayStorage.storedOperator = (buttonPress == "=") ? "" : buttonPress;
+    displayStorage.isBinaryOperatorLastPressed = (buttonPress != "=");
 }
 
 function add(a, b) {
